bea69b63b680312fe07c6b14ec6c35cdd805c40e
Sergei Krivchenko <s.krivchenko@metrotek.ru>
2022-05-30 09:47:57 +0300

    Temp commit

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include <mqueue.h>
 #include <fcntl.h>
 #include <time.h>
+#include <sys/socket.h>
 
 int stop = 0;
 
@@ -36,32 +37,30 @@ void *accum_stat( void *msg_queue_name ) {
 
   mq_fd = mq_open( msg_queue_name, O_RDONLY | O_NONBLOCK );
 
-  if( errno ) {
+  if( mq_fd == (mqd_t)-1 ) {
     perror("mq_open");
     pthread_exit( NULL );
   }
-  
-  int msg_value;
-  int ret;
+
+  u_int32_t msg_value;
+  int       ret;
 
   while( stop == 0 ) {
     ret = mq_receive( mq_fd, (char*)&msg_value, 4, NULL );
-    if( errno == EAGAIN ) {
+    if( ret != -1 ) {
+      printf("Message value: %0d\n", msg_value);
+    } else if( errno == EAGAIN ) {
       sleep(1);
-    } else if ( errno ) {
+    } else {
       perror("mq_timedreceive");
-      mq_close( mq_fd );
-      //mq_unlink( mq_fd );
+      if( mq_close( mq_fd ) )
+        perror("mq_close");
       pthread_exit( NULL );
     }
-    
-    if( ret != -1 )
-      printf("Message value: %0d\n", msg_value);
   }
   
-  mq_close( mq_fd );
-  mq_unlink( mq_fd );
-  printf("Thread was closed\n");
+  if( mq_close( mq_fd ) )
+    perror("mq_close");
   pthread_exit( NULL );
 }
 
@@ -81,9 +80,11 @@ int main ( int argc, char *argv[] ) {
 
   char msg_queue_name [12] = "/test-msg-q\0";
 
+  int raw_socket = socket(SOCK SOCK_DGRAM, htons(3));
+
   mq_fd = mq_open( msg_queue_name, O_WRONLY | O_CREAT, 0200, &attr );
 
-  if( errno ) {
+  if( mq_fd == (mqd_t)-1 ) {
     perror("mq_open");
     exit(EXIT_FAILURE);
   }
@@ -99,17 +100,22 @@ int main ( int argc, char *argv[] ) {
 
   while( stop == 0 ) {
     ret = mq_send( mq_fd, (char*)&msg_value, 4, 0 );
-    if( errno ) {
+    if( ret == -1 ) {
       perror("mq_send");
-      mq_close( mq_fd );
+      if( mq_close( mq_fd ) )
+        perror("mq_close");
     }
     msg_value++;
     sleep(2);
   }
 
+  if( mq_close( mq_fd ) )
+    perror("mq_close");
+
   pthread_join( accum_thread, NULL );
 
-  mq_unlink( mq_fd );
+  if( mq_unlink( msg_queue_name ) )
+    perror("mq_unlink");
 
   exit(EXIT_SUCCESS);
 }
